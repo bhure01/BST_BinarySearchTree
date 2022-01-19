7d0ac591959ffb607e96eadc45291de03cae4d3b
bhure01 <dattatribhure@gmail.com>
2022-01-20 00:56:57 +0530

    Added: creating a binary search tree

---
+++ src/com/bridgelabz/MyBinaryTreeTest.java
@@ -0,0 +1,19 @@
+package com.bridgelabz;
+
+import static org.junit.Assert.*;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MyBinaryTreeTest {
+
+    @Test
+    void given_3Numbers_When_Added_ToBinaryTree_ShoudReturnSizeThree() {
+        MyBinaryTree<Integer> myBinaryTree = new MyBinaryTree<>();
+        myBinaryTree.add(56);
+        myBinaryTree.add(30);
+        myBinaryTree.add(70);
+        int size = myBinaryTree.getSize();
+        Assert.assertEquals(2, size);
+    }
+}
